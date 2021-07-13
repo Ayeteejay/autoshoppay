fff8cc677e7912842dfcfa779a5ffafb3929da4f
Adam Jones <adam@ayeteejay.com>
2021-07-13 10:04:57 -0700

    Adding in Font Awesome fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-fontawesome-css",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
