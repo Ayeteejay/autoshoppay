3e7a8b988c23add71a7bda0550977af269d3b0d4
Adam Jones <adam@ayeteejay.com>
2021-08-03 10:24:12 -0700

    Testing build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -180,7 +180,7 @@ const Footer = () => {
                             <li>Product</li>
                             {props.allWpMenu.edges[0].node.menuItems.nodes.map((value)=>{
                                 return (
-                                    <li key={value.id}><a href={"/" + value.url} className="footer">{value.label}</a></li>
+                                    <li key={value.id}>{value.label}</li>
                                 )
                             })}
                         </ul>
