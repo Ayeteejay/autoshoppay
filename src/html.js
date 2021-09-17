7c9809d6bc1d5b91cd964e8693cdf3f7e8a9b853
Adam Jones <adam@vsslagency.com>
2021-09-17 14:56:42 -0700

    Adding in Hot Jar tracking code

--- src/html.js
+++ src/html.js
@@ -12,17 +12,24 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-
+        {/* Salesloft Tracking */}
         <script dangerouslySetInnerHTML={{__html: `
-                    (function(i,s,o,g,r,a,m){i['SLScoutObject']=r;i[r]=i[r]||function(){
-                      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                      })(window,document,'script','https://scout-cdn.salesloft.com/sl.js','slscout');
-                      slscout(["init", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0IjoxMDQwMzd9.xvmZbjPQnCg3cL9RokyVHC8YFL3rN7An5uOSQ85TwJk"]);
-
-                      console.log("this is work");
-                    
-                    
+            (function(i,s,o,g,r,a,m){i['SLScoutObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://scout-cdn.salesloft.com/sl.js','slscout');
+              slscout(["init", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0IjoxMDQwMzd9.xvmZbjPQnCg3cL9RokyVHC8YFL3rN7An5uOSQ85TwJk"]);                        
+        `}}/>
+        {/* Hot Jar Tracking */}
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2512284,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');                 
         `}}/>
         
       </head>
@@ -47,3 +54,5 @@ HTML.propTypes = {
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
 }
+
+
